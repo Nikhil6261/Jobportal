82ba75394eac9b7e20d83185cee5eb6dd5eca7b6
Nikhil6261 <Nikkuahirwar2@gmail.com>
2025-06-17 08:46:35 +0530

    .

--- backend/src/db/db-config.js
+++ backend/src/db/db-config.js
@@ -1,12 +1,13 @@
 import mysql2 from 'mysql2/promise'
 
 const db = mysql2.createPool({
-    host: '${{ MySQL.MYSQL_URL }}',
+    host: ' mysql://root:hsmGSaisuloRnobIZKDeGqwCsqqAKXWD@maglev.proxy.rlwy.net:14471/railway ',
     user: 'root',
-    password: 'Nikhil@6261',
-    database: 'userjob',
-    port: 3306
+    password: 'hsmGSaisuloRnobIZKDeGqwCsqqAKXWD',
+    database: 'job_protal',
+    port: 14471
 })
 
+// mysql://root:hsmGSaisuloRnobIZKDeGqwCsqqAKXWD@maglev.proxy.rlwy.net:14471/railway
 
 export default db
