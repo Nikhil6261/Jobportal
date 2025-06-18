2ad01c8e0c6eb76c29f8fd15b987ff742b693475
Nikhil6261 <Nikkuahirwar2@gmail.com>
2025-06-18 19:46:13 +0530

    db

--- backend/src/db/db-config.js
+++ backend/src/db/db-config.js
@@ -1,14 +1,12 @@
 import mysql2 from 'mysql2/promise'
 
 const db = mysql2.createPool({
-    host: "localhost",
-    user: "root",
-    password: 'Nikhil@6261',
-    database:'userjob',
-    port:3306
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port:process.env.MYSQLPORT
 })
-
-
 
 
 export default db
